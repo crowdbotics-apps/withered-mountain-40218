71e33f7dcadd38e32860cad899b90cc0de01d327
Crowdbotics <team@crowdbotics.com>
2023-04-25 23:16:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-mountain-40218",
+  url: "https://withered-mountain-40218.botics.co",
+  api: "https://withered-mountain-40218.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
